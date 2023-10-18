eae12179aa52c26695072b765ef82a2d639b342f
George Polyansky <83121990+ImING0@users.noreply.github.com>
2023-10-18 18:40:40 +0300

    feat
    добавлено логирование в контроллеры

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.item.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.dto.ItemDto;
@@ -16,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/items")
 @RequiredArgsConstructor
+@Slf4j
 public class ItemController {
 
     private final String REQUEST_HEADER = "X-Sharer-User-Id";
@@ -24,12 +26,14 @@ public class ItemController {
     @PostMapping
     public ResponseEntity<ItemDto> createItem(@RequestHeader(REQUEST_HEADER) Long userId,
             @RequestBody @Valid Item item) {
+        log.info("createItem request: userId = {}, item = {}", userId, item);
         return ResponseEntity.ok(itemService.createItem(userId, item));
     }
 
     @PatchMapping("/{itemId}")
     public ResponseEntity<ItemDto> updateItem(@RequestHeader(REQUEST_HEADER) Long userId,
             @PathVariable Long itemId, @RequestBody Item item) {
+        log.info("updateItem request: userId = {}, itemId = {}, item = {}", userId, itemId, item);
         return ResponseEntity.ok(itemService.updateItem(userId, itemId, item));
     }
 
@@ -37,23 +41,27 @@ public class ItemController {
     public ResponseEntity<Void> deleteItem(@RequestHeader(REQUEST_HEADER) Long userId,
             @PathVariable Long itemId) {
         itemService.deleteItem(userId, itemId);
+        log.info("deleteItem request: userId = {}, itemId = {}", userId, itemId);
         return ResponseEntity.ok()
                 .build();
     }
 
     @GetMapping("/{itemId}")
     public ResponseEntity<ItemDto> getItemById(@PathVariable Long itemId) {
+        log.info("getItemById request: itemId = {}", itemId);
         return ResponseEntity.ok(itemService.getItemById(itemId));
     }
 
     @GetMapping
     public ResponseEntity<List<ItemDto>> getAllOwnerItemsByOwnerId(
             @RequestHeader(REQUEST_HEADER) Long ownerId) {
+        log.info("getAllOwnerItemsByOwnerId request: ownerId = {}", ownerId);
         return ResponseEntity.ok(itemService.getAllOwnerItemsByOwnerId(ownerId));
     }
 
     @GetMapping("/search")
     public ResponseEntity<List<ItemDto>> searchItem(@RequestParam("text") String text) {
+        log.info("searchItem request: text = {}", text);
         return ResponseEntity.ok(itemService.searchItem(text));
     }
 }
--- src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.dto.UserDto;
@@ -13,35 +14,41 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/users")
+@Slf4j
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping
     public ResponseEntity<UserDto> createUser(@Valid @RequestBody User user) {
+        log.info("createUser request: user = {}", user);
         return ResponseEntity.ok(userService.createUser(user));
     }
 
     @PatchMapping("/{userId}")
     public ResponseEntity<UserDto> updateUser(@PathVariable("userId") Long userId,
             @RequestBody User user) {
+        log.info("updateUser request: userId = {}, user = {}", userId, user);
         return ResponseEntity.ok(userService.updateUser(userId, user));
     }
 
     @DeleteMapping("/{userId}")
     public ResponseEntity<Void> deleteUser(@PathVariable("userId") Long userId) {
         userService.deleteUser(userId);
+        log.info("deleteUser request: userId = {}", userId);
         return ResponseEntity.ok()
                 .build();
     }
 
     @GetMapping("/{userId}")
     public ResponseEntity<UserDto> getUserById(@PathVariable("userId") Long userId) {
+        log.info("getUserById request: userId = {}", userId);
         return ResponseEntity.ok(userService.getUserById(userId));
     }
 
     @GetMapping
     public ResponseEntity<List<UserDto>> getAllUsers() {
+        log.info("getAllUsers request");
         return ResponseEntity.ok(userService.getAllUsers());
     }
 }
